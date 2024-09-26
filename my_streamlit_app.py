3f7ef1b95b280fe770d21e4a493a3922241cda8a
riahi-rami <riahirami1@gmail.com>
2024-09-26 18:11:38 +0200

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -77,7 +77,12 @@ def add_bg_from_local(image_file):
     .stApp {{
         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
         background-size: cover;
-        opacity: 0.2;
+         background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: local;
+        background-color: rgba(0, 0, 0, 0.5);
+        color: white;
+    }}
     }}
     </style>
     """,
